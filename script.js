591b611a5f4a214ee2f25f08bf9b0618847c3217
MyUsername <my@email.com>
2021-10-20 16:02:29 +0200

    commit

--- script.js
+++ script.js
@@ -4,6 +4,4 @@ let lastname = prompt("qual'è il tuo cognome?");
 
 let color = prompt("qual'è il tuo colore preferito?");
 
-let password = "name" + "lastname" + "color";
-
-document,getElementById("password").innerHTML = `La password assegnata è: ${password}`
+document,getElementById("password").innerHTML = `La password assegnata è: ${lastname}`;
